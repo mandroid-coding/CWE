25bab56d7fd9709538ac8ecf683e97e808666c5e
calvint <calvin.troutt@ncf.edu>
2012-05-03 10:13:48 -0400

    updated terrain

--- res/imports/CWE_terrain.py
+++ res/imports/CWE_terrain.py
@@ -4,48 +4,57 @@ class Terrain(object):
 	def __init__(self, square, defense):
 		self.square = square
 		self.defense = defense
+		self.label = "None"
+	def __str__(self):
+		return self.label
 
 class Forest(Terrain):
 	def __init__(self, square):
 		Terrain.__init__(self, square, 2)
 		self.color = 'darkgreen'
+		self.label = "Forest"
 		
 class Plains(Terrain):
 	def __init__(self, square):
 		Terrain.__init__(self, square, 1)
 		self.color = "lightgreen"
+		self.label = 'Plains'
 		
 class Mountain(Terrain):
 	def __init__(self, square):
 		Terrain.__init__(self, square, 4)
 		self.color = "chocolate"
+		self.label = "mountain"
 		
 class Sea(Terrain):
 	def __init__(self, square):
 		Terrain.__init__(self, square, 1)
 		self.color = "blue"
+		self.label = "Sea"
 		
 class Reef(Terrain):
 	def __init__(self, square):
 		Terrain.__init__(self, square, 2)
 		self.color = "turquoise"
+		self.label = "Reef"
 		
 class River(Terrain):
 	def __init__(self, square):
 		Terrain.__init__(self, square, 0)
 		self.color = "lightblue"
-	
+		self.label = "River"
+		
 class Road(Terrain):
 	def __init__(self, square):
 		Terrain.__init__(self, square, 0)
 		self.color = "grey90"
-
+		self.label = "Road"
+		
 class Beach(Terrain):
 	def __init__(self, square):
 		Terrain.__init__(self, square, 0)
 		self.color = "tan"
-
-	
+		self.label = "Beach"
 		
 class Building(Terrain):
 	def __init__(self, square, defense, controller = False):
@@ -64,19 +73,22 @@ class Building(Terrain):
 class City(Building):
 	def __init__(self, square, controller):
 		Building.__init__(self, square, 3, controller)
+		self.label = "City"
 		
 class Base(Building):
 	def __init__(self, square, controller):
 		Building.__init__(self, square, 3, controller)
+		self.label = "Base"
 		
 class Airport(Building):
 	def __init__(self, square, controller):
 		Building.__init__(self, square, 3, controller)
+		self.label = "Airport"
 		
 class Port(Building):
 	def __init__(self, square, controller):
 		Building.__init__(self, square, 3, controller)
-	
+		self.label = "Port"
 
 	
 	
