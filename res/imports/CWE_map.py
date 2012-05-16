153e6c8b5bd30bfed60a8cb59ff70c9e9912515d
calvint <calvin.troutt@ncf.edu>
2012-05-16 16:39:44 -0400

    map check code

--- res/imports/CWE_map.py
+++ res/imports/CWE_map.py
@@ -57,8 +57,8 @@ class Map(object):
 		#self.get_square([1,8]).add_unit(Unit("infantry.unit", square = self.get_square([1,8]), player = player_list[0]))
 		#self.get_square([8,1]).add_unit(Unit("infantry.unit", square = self.get_square([8,1]), player = player_list[1]))
 		self.get_square([2,7]).add_terrain(CWE_terrain.Base(self.get_square([2,7])))
-		self.get_square([2,7]).add_terrain(CWE_terrain.Base(self.get_square([2,7]), self.player_list[0]))
-		self.get_square([2,7]).add_terrain(CWE_terrain.Base(self.get_square([2,7]), self.player_list[1]))
+		self.get_square([4,5]).add_terrain(CWE_terrain.Base(self.get_square([4,5]), self.player_list[0]))
+		self.get_square([7,2]).add_terrain(CWE_terrain.Base(self.get_square([7,2]), self.player_list[1]))
 		
 	
 	def create_grid(self, grid_width_and_height):
@@ -203,27 +203,27 @@ def code_check():
 	y = 0
 	print "test of find_rage(1,3):", x.squares[3][3].find_unrestricted_range(1)
 	print "test of square.distance_to (should be 5) :", x.squares[3][3].distance_to(x.squares[6][5])
-	print "test of terrain population:"
-	for list_of_squares in x.squares:
-		for square in list_of_squares:
-			print str(square.terrain)[13:14] , 
-			y += 1
-			if y == 10:
-				y = 0
-				print ""
+	print "test of print_map: \n", print_map(x)
 
 def print_map(a_map):
-	print "Printing map to console.\nBeep beep, boop boop..."
+	print "Printing map to console.\n\nBeep beep, boop boop..."
 	counter = 0
-	for x in a_map.squares:
-		# this is supposed to iterate down columns
-		for y in x:
-			# but it prints down rows
-			print str(y.terrain)[13:14] , 
+	for x in range(len(a_map.squares)):
+		for y in range(len(a_map.squares[0])-1, -1, -1):
 			counter += 1
-			if counter == 10:
+			print a_map.get_square([x,y]).terrain.label[0:1],
+			if counter == len(a_map.squares):
 				counter = 0
 				print ""
+	#for x in a_map.squares:
+		## this is supposed to iterate down columns
+		#for y in x:
+			## but it prints down rows
+			#print str(y.terrain)[10:14] , 
+			#counter += 1
+			#if counter == 10:
+				#counter = 0
+				#print ""
 
 
 
